86666025b1d38d2803f4f40dfe1c8ac20ce178c3
EC2 Default User <ec2-user@ip-172-31-37-95.ec2.internal>
2022-09-02 13:42:35 +0000

    finished all task

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=iamfydel/devops_microservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops-microservices\
+	--image=$dockerpath\
+	--port=80 --labels app=devops-microservices
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops-microservices 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
@@ -5,11 +7,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=iamfydel/devops_microservices
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag devops_microservices $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
